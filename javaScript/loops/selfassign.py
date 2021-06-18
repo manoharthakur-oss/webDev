44e2ac04b9d3107de4bbc71a5e49bfcc5f8494d8
Manohar <tsmile2459@gmail.com>
2021-06-18 15:26:33 +0530

    more

---
+++ javaScript/loops/selfassign.py
@@ -0,0 +1,11 @@
+SELF ASSIGNMENT:
+	- when we set variable to its own value.
+	- 
+	- self assigning the variable help us to increase the value of the same variable.:
+		eg ; w = w + 1
+	- self assignment of variable let us to track the change in data over time.
+	- 
+
+
+
+
